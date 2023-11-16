061a2834528d4937d5f998f6cb62995ac91ed22f
Airat Khisamiev <aira3t@gmail.com>
2023-11-16 22:59:19 +0100

    Filter by status latest sentence

--- src/sentences/sentences.service.ts
+++ src/sentences/sentences.service.ts
@@ -104,6 +104,7 @@ export class SentencesService {
               status: 'EXECUTED',
               userId: user.id,
               id: sentenceId,
+              status: 'EXECUTED',
             },
       orderBy: {
         createdAt: 'desc',
